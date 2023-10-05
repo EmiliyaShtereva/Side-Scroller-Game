839be2f11d9318f17b76925a67cc31dbf312d3d6
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-10-05 15:18:11 +0300

    added html js and css files, drew a canvas for the gameplay

---
+++ main.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth - 15;
+    canvas.height = window.innerHeight - 15;
+});
