f2b92ab20660562038b7be1c562f7d5eb504c664
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2023-10-07 09:42:25 +0300

    added horizontal movement of the player

--- player.js
+++ player.js
@@ -4,13 +4,26 @@ export class Player {
         this.width = 72.5;
         this.height = 86;
         this.position = {
-            x: 0,
+            x: 200,
             y: this.game.height - this.height
         }
+        this.image = '';
         this.idleImage = document.getElementById('idlePlayer');
+        this.speed = 0;
+        this.maxSpeed = 7;
     }
-    update() {
-        // this.position.x++;
+    update(input) {
+        // horizontal movement
+        this.position.x += this.speed;
+        if (input.includes('d')) this.speed = this.maxSpeed;
+        else if (input.includes('a')) this.speed = -this.maxSpeed;
+        else this.speed = 0;
+
+        if (this.position.x < 100) this.position.x = 100;
+        if (this.position.x > 450) this.position.x = this.position.x = 450;
+        
+        // vertical movement
+
     }
     draw(context) {
         context.drawImage(this.idleImage, 0, 0, this.width, this.height, this.position.x, this.position.y, this.width, this.height)
